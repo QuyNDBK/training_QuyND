a97c582c4492f3811398e3b27a18ce5fb136b791
Dinh Quy <quynd@rikkeisoft.com>
2017-06-27 09:13:44 +0700

    27-06 lan dau

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix'=>'admin'],function(){
+        Route::get('danhsach','DanhSachController@getDanhSach');
+        
+	Route::group(['prefix'=>'theloai'],function(){
+
+		Route::get('danhsach','TheLoaiController@getDanhSach');
+
+		Route::get('sua/{id}','TheLoaiController@getSua');
+		Route::post('sua/{id}','TheLoaiController@postSua');
+
+		Route::get('them','TheLoaiController@getThem');
+		Route::post('them','TheLoaiController@postThem');
+
+		Route::get('xoa/{id}','TheLoaiController@postXoa');
+	});
+
+	    
+});
